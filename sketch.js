143f1fb4d773c541f551aa4e3fd26eaed3d300c7
manami000 <90057527+manami000@users.noreply.github.com>
2025-01-30 00:34:43 +0100

    Update sketch.js_prova

--- sketch.js
+++ sketch.js
@@ -1,52 +1,39 @@
 let size1 = 100;
-let size2 = 1214*0.75;
-let size3 = 750*0.75;
 let counter = 1;
+let alien1, bag;
 
-
-
-function preload()
-{
+function preload() {
   alien1 = loadImage("alien3.png");
   bag = loadImage("bag.png");
 }
 
-
 function setup() {
-  createCanvas(1214*0.75, 750*0.75);
+  createCanvas(windowWidth, windowHeight); // Make canvas fit the iframe size
   background(bag);
-  alien1.resize(size1, size1);
-  bag.resize(size2, size3);
-
-  noCursor();
   
+  alien1.resize(size1, size1);
+  bag.resize(windowWidth, windowHeight);
+  noCursor();
 }
 
 function draw() {
-  
-  // background(bag,5);
-  if (counter == 1)
-    {
-      image(alien1, mouseX-size1/2, mouseY-size1/2)
-    }
-
-
-  
-    
-  
+  if (counter == 1) {
+    image(alien1, mouseX - size1 / 2, mouseY - size1 / 2);
+  }
 }
 
-function mouseClicked()
-{
+function mouseClicked() {
   clear();
   background(255);
-  if (counter == 1)
   
-  {
-      counter = 1;
+  if (counter == 1) {
+    counter = 1;
     background(bag);
-    }
-  
+  }
 }
-  
-  
+
+// Resize canvas when the window size changes
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  background(bag);
+}
