b774ca681b47455ab904829e4b19139c8910fd90
manami000 <90057527+manami000@users.noreply.github.com>
2025-01-29 23:59:54 +0100

    Update sketch.js_resize

--- sketch.js
+++ sketch.js
@@ -1,4 +1,6 @@
 let size1 = 150;
+let size2 = 2428/2;
+let size3 = 1500/2;
 let counter = 1;
 
 
@@ -14,6 +16,7 @@ function setup() {
   createCanvas(2428/2, 1500/2);
   background(bag);
   alien1.resize(size1, size1);
+  bag.resize(size2, size3);
 
   noCursor();
   
