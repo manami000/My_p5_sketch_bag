1564214a30ce3d908787c341ed775e33f1cb849b
manami000 <90057527+manami000@users.noreply.github.com>
2025-01-29 23:53:49 +0100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,49 @@
+let size1 = 150;
+let counter = 1;
+
+
+
+function preload()
+{
+  alien1 = loadImage("alien3.png");
+  bag = loadImage("bag.png");
+}
+
+
+function setup() {
+  createCanvas(2428, 1500);
+  background(bag);
+  alien1.resize(size1, size1);
+
+  noCursor();
+  
+}
+
+function draw() {
+  
+  // background(bag,5);
+  if (counter == 1)
+    {
+      image(alien1, mouseX-size1/2, mouseY-size1/2)
+    }
+
+
+  
+    
+  
+}
+
+function mouseClicked()
+{
+  clear();
+  background(255);
+  if (counter == 1)
+  
+  {
+      counter = 1;
+    background(bag);
+    }
+  
+}
+  
+  
